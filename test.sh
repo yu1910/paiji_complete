a1e94571b60ef7bc5989f53039e615f470ed12d3
yuyongpeng <yuyongpeng@novogene.com>
2026-04-15 10:09:53 +0800

    平衡文库修复+排机问题修复

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 # 请用 bash 执行：bash test.sh 或 chmod +x test.sh && ./test.sh（不要用 python 运行本文件）
 python "./arrange_library/arrange_library_model6.py" \
   --mode arrange \
-  --data-file "./33_sta.csv" \
-  --output-file "./33_sta_arrange.csv"
+  --data-file "./53_sta.csv" \
+  --output-file "./53_sta_arrange.csv"
